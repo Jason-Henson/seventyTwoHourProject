f75a56a19ff10b03d7c64c9037932ce8c6f80ae1
Jason-Henson <jason.a.henson@gmail.com>
2021-08-27 22:02:00 -0400

    data is pulling correctly still needs toggle button and styling

--- 72-hr-project/src/App.js
+++ 72-hr-project/src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
 import Weather from './weather/weather';
-import Nasa from './nasa/Nasa';
-import Tickets from './tickets/Tickets';
+import React, { useState, useEffect } from 'react';
+// import Nasa from './nasa/Nasa';
+// import Tickets from './tickets/Tickets';
 
 function App() {
+
+  const [lon, setLon] = useState(0)
+  const [lat, setLat] = useState(0)
+  const getLoc = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude)
+      setLon(position.coords.longitude)
+      localStorage.setItem("setLat",position.coords.latitude)
+      localStorage.setItem("setLon",position.coords.longitude)
+      console.log(lat);
+      console.log(lon)
+    })
+  }
+  useEffect(() => {
+    getLoc()
+  }, [])
+
   return (
     <div>
-      <Nasa />
-      <Tickets />
-      <Weather />
+      {/* <Nasa />
+      <Tickets /> */}
+      <Weather lat={lat} lon={lon} />
     </div>
   );
 }
--- 72-hr-project/src/weather/weather.jsx
+++ 72-hr-project/src/weather/weather.jsx
@@ -1,9 +1,55 @@
-const Weather = (props) => {
-    return ( 
-        <div>
-            <h1>Hello from weather</h1>
-        </div>
-     );
+import { Button } from 'bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container, Col, Row } from 'reactstrap'
+
+const Weather = (props) => { 
+    
+    let lat = props.lat === 0 ? localStorage.getItem("setLat") : props.lat
+    let lon = props.lat === 0 ? localStorage.getItem("setLon") : props.lon
+
+    // *** Fetch ***   
+
+    const apiKey = `03c1666c3ae04bf692763ac28385b961`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+
+
+    // state variables to contain fetched data
+    const [temp, setTemp] = useState([]);
+    const [feelsLike, setFeelsLike] = useState([]);
+    const [highTemp, setHighTemp] = useState([]);
+    const [lowTemp, setLowTemp] = useState([]);
+    const [humidity, sethumidity] = useState([]);
+    const [userLoc, setUserLoc] = useState('');
+
+    // API endpoiont and key appended with imerial units of measurement
+
+    const fetchWeather = async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        setTemp(data.main.temp);        
+        setFeelsLike(data.main.feels_like);
+        setHighTemp(data.main.temp_max);
+        setLowTemp(data.main.temp_min);
+        sethumidity(data.main.humidity);
+        setUserLoc(data.name);
+    }  
+
+    useEffect(() => {
+        fetchWeather()
+    }, [])
+
+    // Return
+    return (        
+        <Container>       
+            Your Locaiton: {userLoc} <br />
+            Current Temp: {temp}&#8457; <br />
+            Heat Index: {feelsLike}&#8457; <br />
+            High Temp: {highTemp}&#8457; <br />
+            Low Temp: {lowTemp}&#8457; <br />
+            Humidity: {humidity}&#37; <br />
+        </Container>      
+    );
 }
  
 export default Weather;
